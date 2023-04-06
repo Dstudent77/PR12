6117f3a72a235404a40a8121422e6ecf5dbd056d
Dstudent77 <kuzminchanel@gmail.com>
2023-04-06 09:47:39 +0300

    Add tests to GH Actions

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,5 +6,28 @@ def test_healthcheck():
     response = requests.get(f'{api_url}/__health')
     assert response.status_code == 200
 
-#class TestDocuments():
+class TestDocuments():
+    def test_empty_docs():
+        response = requests.get(f'{api_url}/v1/docs')
+        assert response.status_code == 200
+        assert len(response.json()) == 0
+        
+    def test_create_docs():
+        body = { "title": "New title", "body": "Prosto text" }
+        response = requests.post(f'{api_url}/v1/docs', json=body)
+        assert response.status_code == 200
+        assert response.json().get('title') == 'New title'
+        assert response.json().get('body') == 'Prosto text'
+        assert response.json().get('id') == 0
     
+    def test_get_doc_by_id():
+        response = requests.get(f'{api_url}/v1/docs/0')
+        assert response.status_code == 200
+        assert response.json().get('title') == 'New title'
+        assert response.json().get('body') == 'Prosto text'
+        assert response.json().get('id') == 0
+        
+    def test_empty_docs():
+        response = requests.get(f'{api_url}/v1/docs')
+        assert response.status_code == 200
+        assert len(response.json()) == 1
